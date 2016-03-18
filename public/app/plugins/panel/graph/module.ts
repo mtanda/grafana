90be9bb7e856ff869ce18f4d7a52e44e0564ae55
Mitsuhiro Tanda <mitsuhiro.tanda@gmail.com>
2016-03-18 19:43:56 +0900

    highlight series line by hover legend

--- public/app/plugins/panel/graph/module.ts
+++ public/app/plugins/panel/graph/module.ts
@@ -87,6 +87,7 @@ class GraphCtrl extends MetricsPanelCtrl {
   static template = template;
 
   hiddenSeries: any = {};
+  highlightSeries: any = {};
   seriesList: any = [];
   logScales: any;
   unitFormats: any;
@@ -226,6 +227,20 @@ class GraphCtrl extends MetricsPanelCtrl {
       this.toggleSeriesExclusiveMode(serie);
     }
 
+    this.render();
+  }
+
+  toggleHighlightSeries(serie, event) {
+    if (this.highlightSeries[serie.alias] && this.highlightSeries[serie.alias].highlight) {
+      return;
+    }
+
+    this.highlightSeries[serie.alias] = { highlight: true, originalSetting: _.clone(serie.lines) };
+    setTimeout((function() {
+      this.highlightSeries[serie.alias].highlight = false;
+      serie.lines = this.highlightSeries[serie.alias].originalSetting;
+      this.render();
+    }).bind(this), 500);
     this.render();
   }
 
