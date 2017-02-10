9f43313299d46657838e906dbfa911f4960f377c
Mitsuhiro Tanda <mitsuhiro.tanda@gmail.com>
2017-02-10 13:39:22 +0900

    temporally fix for CloudWatch DynamoDB

--- public/app/plugins/datasource/cloudwatch/datasource.js
+++ public/app/plugins/datasource/cloudwatch/datasource.js
@@ -370,6 +370,9 @@ function (angular, _, moment, dateMath, kbn, CloudWatchAnnotationQuery) {
             dps.push([null, lastTimestamp + periodMs]);
           }
           lastTimestamp = timestamp;
+          if (options.namespace === 'AWS/DynamoDB' && options.metricName.indexOf('Consumed') >= 0 && stat === 'Sum') {
+            dp[stat] = dp[stat] / options.period;
+          }
           if (!extended) {
             dps.push([dp[stat], timestamp]);
           } else {
