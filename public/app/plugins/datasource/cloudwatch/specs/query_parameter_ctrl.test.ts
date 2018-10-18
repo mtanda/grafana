5469623c3b8e6002d58b95df88e86c9d3ca4ff17
Mitsuhiro Tanda <mitsuhiro.tanda@gmail.com>
2018-10-18 23:36:46 +0900

    wip

--- public/app/plugins/datasource/cloudwatch/specs/query_parameter_ctrl.test.ts
+++ public/app/plugins/datasource/cloudwatch/specs/query_parameter_ctrl.test.ts
@@ -4,12 +4,54 @@ describe('QueryParameterCtrl', () => {
   const ctx = {} as any;
 
   beforeEach(() => {
-
     ctx.ctrl = new CloudWatchQueryParameterCtrl({}, {}, {}, {}, {});
   });
 
-  describe('generate targetFull', () => {
-    console.log('here');
-    console.log(ctx.ctrl.expressionChanged);
+  describe('should generate correct targetFull', () => {
+    const targets = [
+      {
+        refId: 'A',
+        id: 'id1',
+        region: 'us-east-1',
+        namespace: 'AWS/EC2',
+        metricName: 'CPUUtilization',
+        statistics: [
+          'Average'
+        ]
+      },
+      {
+        refId: 'B',
+        id: 'id2',
+        expression: 'id1*2',
+      },
+      {
+        refId: 'C',
+        id: 'id3',
+        expression: 'id2*2',
+      },
+      {
+        refId: 'D',
+        id: 'id4',
+        region: 'us-west-1',
+        namespace: 'AWS/EC2',
+        metricName: 'CPUUtilization',
+        statistics: [
+          'Average'
+        ]
+      },
+    ];
+    let target: any = {
+      refId: 'C',
+      id: 'id3',
+      expression: 'id2*2',
+    };
+    ctx.ctrl.renderTargetFull(target, targets)
+    expect(target.targetFull.lenght).toBe(3);
+    expect(target.targetFull[0].refId).toBe('A');
+    expect(target.targetFull[0].region).toBe('ap-northeast-1');
+    expect(target.targetFull[1].refId).toBe('B');
+    expect(target.targetFull[1].region).toBe('ap-northeast-1');
+    expect(target.targetFull[2].refId).toBe('C');
+    expect(target.targetFull[2].region).toBe('ap-northeast-1');
   });
 });
